b665cae5b3bfa22aa0278cc6a3647db747ffd93b
Жангельдин Темирлан <@corp.itstep>
2018-08-29 21:36:53 +0600

    Added a new script

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("Введите свое имя: ");
+        }
+    }
+}
